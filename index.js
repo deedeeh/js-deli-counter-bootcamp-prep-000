447cb280f51ca498f1d251d8952173a87cfc5160
Dina Hafez <dinahafez.8@gmail.com>
2018-05-17 20:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
     var newArray = [];
     var result = 'The line is currently: ';
     for(var i = 0; i < katzDeliLine.length; i++) {
-      newArray.push(katzDeliLine[i] + ", ");
+      newArray.push(katzDeliLine[i].join(", "));
       result += `${i+1}. ${newArray[i]}`;
     }
     return result;
