c6d691e34705545ad2d744d9ab15ac22bd483701
Dina Hafez <dinahafez.8@gmail.com>
2018-05-17 21:16:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,15 +29,4 @@ function currentLine(katzDeliLine) {
   }
 }
 
-// function currentLine(katzDeliLine) {
-//   if(katzDeliLine.length === 0) {
-//     return "The line is currently empty.";
-//   } else {
-//     var result = 'The line is currently: ';
-//     for(var i = 0; i < katzDeliLine.length; i++) {
-//       result += `${i+1}. ${katzDeliLine[i]}, `;
-//     }
-//     return result;
-//   }
-// }
 
