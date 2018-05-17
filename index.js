79d3109a56a1046672ee32a770a4f81eca1257d2
Dina Hafez <dinahafez.8@gmail.com>
2018-05-17 20:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeliLine) {
     for(var i = 0; i < katzDeliLine.length; i++) {
       newArray.push(katzDeliLine[i]);
       // newArray.join(", ");
-      result += `${i+1}. ${newArray[i].join(", ")}`;
+      result += `${i+1}. ${newArray.join(", ")}`;
     }
     return result;
   }
